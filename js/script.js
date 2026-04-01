0d408f1527f57cda66a7b74248f2fd998d25de02
alexandrelopes <alexandresgl@gmail.com>
2026-03-31 21:50:26 -0300

    ajustes

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+function mascaraMoeda(i) {
+    let v = i.value.replace(/\D/g, "");
+
+    if (v.length === 0) {
+        i.value = "";
+        return;
+    }
+
+    v = (Number(v) / 100).toFixed(2);
+
+    let partes = v.split(".");
+    partes[0] = partes[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+    i.value = partes.join(",");
+}
+
+function formatarReal(valor) {
+
+    let numero = parseFloat(valor);
+
+    if (isNaN(numero)) return "0,00";
+
+    return numero.toLocaleString('pt-BR', {
+        minimumFractionDigits: 2,
+    });
+}
+
+function mascaraAltura(i) {
+    let v = i.value.replace(/\D/g, "");
+    if (v.length > 1) {
+        v = v.substring(0, 1) + "." + v.substring(1);
+    }
+    i.value = v;
+}
